dbad5c18ad6b96880c2eaafc6d451117f7cbacff
Your Name <your_email@example.com>
2026-05-27 07:28:49 +0300

    test api

--- src/pages/paths.ts
+++
@@ -1,9 +0,0 @@
-export const paths = {
-  loanCalculator: "/",
-  iscoreCodes: "/iscore-codes",
-  customers: "/customers",
-  addClient: "/add-client",
-  viewClient: "/view-client",
-  installments: "/installments",
-  editInstallment: "/edit-installment",
-} as const;
